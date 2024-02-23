6a404f15368e79b6d27e5c3bac8963d8888019ca
MystMefisto <mefisto@MSI>
2024-02-23 17:47:10 -0600

    Featuring styles to menuprincipal

--- front-page.php
+++ front-page.php
@@ -2,8 +2,45 @@
     get_header();
 ?>
 
-<main>
+<main class='frontpage-container'>
 <h1>Desde pagina de inicio</h1>
+
+<div class="wrapper">
+  <div class="slider" id="slider">
+    <ul class="slides">
+      <li class="slide" id="slide1">
+        <a href="#">
+          <p class="caption">Texto llamativo</p>
+          <img src="https://lh3.googleusercontent.com/fife/AGXqzDlho1ihrKn2CBDfB7Wdu-pneR_vW5o7aAj89H20molZHAgwNHvBY3K1m5tdEpNxdRARlv97PD1OQJL9-vnmLOq6IG3GPD5Ubmu-ONa4dNW0pZQ8_CLfqU2maHHKqGFehQBVOBVzmMzkp_6ZZqCyJLnSB7WQYdM2CGp68TJ69mKvGUTNaqTUpmAR50hlBV33dl71RSG3lrgTPlI98bXRqdsCfYNTRqAFs5zMfh-PkDrBrywWPYcCNLstHePFQjpaVFAqhKfSi3QYTvDgGuFKZgDr8momJRgj5ChFzPmnQhgHh2Up1b7E66tB0DlGKohtBU8lw33oms37TBzPzRyGUQd0C0xpIRX2k3KcKQCfaESr0yuxFJDLb5XBIcDEID6R2Ga1CpOMrQwPraOZJyDDPB6tUbxDvPkwVsNntuugAJExNcjrgUTv5CQ4tm5Wx7TWLfBtPzBZiOLhsRZwPuUBKjyLFg3ayas3k-oyQYdnxKFHuKsv-I3rOfYBjTOctN-uZxPWTuTHzjjOcl7n3ti80hGFkr1wAeU0meQPDp21-5cGpglEbOgXxUuR2rJdvYUtBVjwEZaPjcOY2aEr9EW1iMa6zLZ_nXMVF1nHuWWqCOBSJ0uj1IzwS_HF3XUiwSJcCdkEMfZthtadRJsAaIZf8_Vb1SXHNy1Qk4TRBkLgc5xbOLMExAjxc1XkIGu_88rXDK1ggOC-cHQT9gMNoiqs1uURvVefbilikT5yITsqB2OWjXjVn02nvaZSpF5SE3qyqqlM-sQ-8AVkQ-IGKI9vpVGULKGfYXCakPuK-dRXFX71ZGH_eisADNnaogehUGJu5-7XU-bBJ7gDC6xPfuv4hX3SsyKVrwrRYLYtTe3RmVbMsymdzaKE5ium56BwD6e8TXFdSYV_8p4doUNnhLXsglplhwfV32SPWUJSfin-pRGcXaFVye7ZhMvd1t_lf-oHYyhefvdN2aA6yB-3PSmepzVy488htuLOcIny_puVT6Xg2Iig0y0CfpMo16vGTP-RiNCCwACOdf8Eq5wJ-QPq2Ds_KK-J7tp3I8FraKUx5j_V4KfvkrhD9Ri6Cn6unW3uRs5XE9xvTOSxMoS9BOAn9a6KRGki4O2WXWOKzTZYU1INeXeL9BvJ4oE7kPwOVrC66pfBBo4fQRVpLzgCETOKc8iH2dHbV2z8nW5f032yUhghDuAM3RbsVtVQh2Zo7s7utFXVop85RebgnZEK4rv2GBmi_FOshuwkRs__wRkk2pZXtFA3SvOY1szbJR5Cb5zjB32Y9_ZMU96zq4HkD-ihjc9qoX0bpZ8hTGJoySADgGw_SdAMKA3ldXJjzB_pzHri0BbgQ86Voewh177CWbU9A-ciGaQ0lFo5vgxTSowsCzjJ0de32y_GsP52S1Y66grYxXHkU831aw3MTsyykLShc_WreauyMDsK3TjPG5ypOBV8FlUiASAxI1McwdujbEZ8Qb4gNTEdYKSNOFwWcVv4gf1_ucrbc2qVFa1y1ioFyggwCtR1FTAqNClSrOoKfLngJaqissTRBxsu=s220?authuser=0" alt="Faby">
+        </a>
+      </li>
+      <li class="slide" id="slide2">
+        <a href="#">
+          <p class="caption">Texto llamativo</p>
+          <img src="./SliderPics/Frame1.png" alt="photo 2">
+        </a>
+      </li>
+      <li class="slide" id="slide3">
+        <a href="#">
+          <p class="caption">Texto llamativo</p>
+          <img src="./SliderPics/Frame1.png" alt="photo 3">
+        </a>
+      </li>
+      <li class="slide">
+        <a href="#">
+          <p class="caption">Texto llamativo</p>
+          <img src="photo1.jpg" alt="photo 1">
+        </a>
+      </li>
+    </ul>
+    <ul class="slider-controler">
+      <li><a href="#slide1">&bullet;</a></li>
+      <li><a href="#slide2">&bullet;</a></li>
+      <li><a href="#slide3">&bullet;</a></li>
+    </ul>
+  </div>
+</div>
+
 </main>
 
 <?php
